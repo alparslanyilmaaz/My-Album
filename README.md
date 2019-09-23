122bbb2f6725c59fe68b04422447e6b1f5382125
alparslanylmaz <alparslanylmaz@gmail.com>
2019-09-23 21:08:54 +0200

    bug fix

--- README.md
+++ README.md
@@ -9,4 +9,18 @@ image will be load.
 ## Deployment
 https://photo-album-zingat.herokuapp.com with this link you will access the project without download to your local.
 
+## Screens
+Login Screen  
+<img src="https://github.com/alparlanylmaz/My-Album/blob/master/git-photos/login-page.png"/>  
+Homepage Screen
+<img src="https://github.com/alparlanylmaz/My-Album/blob/master/git-photos/Screenshot%20at%202019-09-22%2023-18-17.png"/>
+
 ## Installation
+
+In here you will see the project structure :  
+<img src="https://github.com/alparlanylmaz/My-Album/blob/master/git-photos/screen.png"/>
+  
+    
+Here is the json file in project  
+<img src="https://github.com/alparlanylmaz/My-Album/blob/master/git-photos/real_json.png"/>  
+You can install this elements with npm install
--- public/dataGetter.js
+++ public/dataGetter.js
@@ -5,8 +5,8 @@ window.addEventListener("load", function(){
         if (XHR.readyState == 4 && XHR.status == 200) {
             for (var i = 0; i < 10; i++) { // 10 id will be get with this for loop
                 document.getElementById("left-albums-list").innerHTML +=    //here we are adding the 10 album id to our list and showing to user
-                `<li id="${i}" class="general-album" > 
-                    <a id="${i}" class="album-text" href="#">  List ${i}</a>
+                `<li id="${i+1}" class="general-album" > 
+                    <a id="${i+1}" class="album-text" href="#">  List ${i+1}</a>
                 </li>`;
             }
         }
@@ -21,10 +21,16 @@ document.getElementById("left-albums-list").addEventListener("click",function(e)
     var XHR = new XMLHttpRequest();
     XHR.onreadystatechange = function(){
         if(XHR.readyState == 4 && XHR.status == 200){
+            if(document.getElementById("images-load") != null){
+                document.getElementById("images-load").innerHTML = "";
+            }
             var photos = JSON.parse(XHR.responseText);
-
+            photos = photos.filter(function(item){
+                return item.albumId === parseInt(e.target.id);
+            })
             if(window.innerWidth < 960){    //if device width smaller than 960 we will get only 4 data
                 for(var i=0; i<4 ; i++){
+                    
                     document.getElementById("first-row").innerHTML +=
                     `<div class="card-general"> 
                         <img id="img${i}" class="img" src="${photos[i].thumbnailUrl}" data-full="${photos[i].url}" onclick="bigger(this)"/>
@@ -50,6 +56,5 @@ document.getElementById("left-albums-list").addEventListener("click",function(e)
 //click event for thumbnails. Image will be get bigger with this.
 function bigger(img){
     document.getElementById("images-load").innerHTML = "";
-    
-    document.getElementById("images-load").innerHTML +=`<img src=${img.getAttribute("data-full")}/>`;
+    document.getElementById("images-load").innerHTML +=`<img class="bigger" src=${img.getAttribute("data-full")}/>`;
 }
